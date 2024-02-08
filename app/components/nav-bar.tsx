51c87b82fc8d7471ddef1d96a5712d9d63e27501
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-02-08 22:20:03 +0530

    updated navbar section

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -1,7 +1,18 @@
 export default function NavBar() {
     return (
         <div>
-            This is the navbar section
+            <div>
+                <img src="#" alt="#" />
+                <p>Nikhil Huirem</p>
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </div>
     )
 }
