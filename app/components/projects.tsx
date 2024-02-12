6d35b05172c0efa66b9d0189a335556e3697f1e3
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-02-12 20:29:33 +0530

    added project page

---
+++ app/components/projects.tsx
@@ -0,0 +1,19 @@
+export default function Projects() {
+  return (
+    <main>
+      <h1>Projects</h1>
+      <section>
+        <h2>Project 1</h2>
+        <p>Project 1 description</p>
+      </section>
+      <section>
+        <h2>Project 2</h2>
+        <p>Project 2 description</p>
+      </section>
+      <section>
+        <h2>Project 3</h2>
+        <p>Project 3 description</p>
+      </section>
+    </main>
+  );
+}
